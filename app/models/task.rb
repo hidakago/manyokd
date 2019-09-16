b5ef187c7efa527dde3fdb82d36a44877929eb76
hidakago <hidakago@gmail.com>
2019-09-16 14:24:26 +0900

    Add:終了期限deadlineのバリデーションの追加およびモデルテスト実施

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,10 @@
 class Task < ApplicationRecord
   validates :name, presence: true
+  validate :not_entered_deadline
+
+  def not_entered_deadline
+    if deadline == "0000-00-00"
+      errors.add(:deadline, "に適切な年月日を設定してください")
+    end
+  end
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -3,12 +3,28 @@ require 'rails_helper'
 RSpec.describe Task, type: :model do
 
   it "nameが空ならバリデーションが通らない" do
-    task = Task.new(name: '', description: '失敗テスト')
+    task = Task.new(name: '', description: '失敗テスト', deadline: '2019-10-19')
     expect(task).not_to be_valid
   end
 
   it "nameに内容が記載されていればバリデーションが通る" do
-    task = Task.new(name: 'テスト', description: '成功テスト')
+    task = Task.new(name: 'テスト', description: '成功テスト', deadline: '2019-10-19')
     expect(task).to be_valid
   end
+
+  it "deadlineが'0000-00-00'ならバリデーションが通らない" do
+    task = Task.new(name: 'テスト3', description: '失敗テスト', deadline: '0000-00-00')
+    expect(task).not_to be_valid
+  end
+
+  it "deadlineが'0000-00-00'でないならバリデーションが通る" do
+    task = Task.new(name: 'テスト4', description: '成功テスト', deadline: '2019-10-19')
+    expect(task).to be_valid
+  end
+
+  it "nameが空、かつdeadlineが'0000-00-00'ならバリデーションが通らない" do
+    task = Task.new(name: '', description: '失敗テスト', deadline: '0000-00-00')
+    expect(task).not_to be_valid
+  end
+
 end
