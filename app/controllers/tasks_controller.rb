f5e070f7a0df16847ce9b1f67c89f870de3dd606
hidakago <hidakago@gmail.com>
2019-09-18 11:00:36 +0900

    Add:priorityの登録機能の追加(項目名は非日本語)

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -63,6 +63,6 @@ class TasksController < ApplicationController
   end
 
   def task_params
-    params.require(:task).permit(:name, :description, :deadline, :status)
+    params.require(:task).permit(:name, :description, :deadline, :status, :priority)
   end
 end
