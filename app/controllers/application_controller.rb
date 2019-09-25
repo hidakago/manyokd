0368660bc6933e4cacc5ba8ab2952797b4704bff
hidakago <hidakago@gmail.com>
2019-09-25 17:26:37 +0900

    Add:ルーティングエラー画面の対応追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
+  def routing_error
+    render file: Rails.root.join('public/404.html'), status: 404, layout: false, content_type: 'text/html'
+  end
 end
