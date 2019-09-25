277747c61df592ef8efbb9f37c477e151a703f1c
hidakago <hidakago@gmail.com>
2019-09-25 22:14:05 +0900

    Add:Labelモデルのseedデータ整備

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,17 @@
-1.times do |n|
-  name = Faker::Games::Pokemon.name
-  email = Faker::Internet.email
-  password = "password"
-  User.create!(name: name,
-               email: email,
-               password: password,
-               password_confirmation: password,
+# 1.times do |n|
+#   name = Faker::Games::Pokemon.name
+#   email = Faker::Internet.email
+#   password = "password"
+#   admin_allowed = false
+#   User.create!(name: name,
+#                email: email,
+#                password: password,
+#                password_confirmation: password,
+#                admin_allowed: false,
+#                )
+# end
+10.times do |n|
+  name = "label_name#{n}"
+  Label.create!(name: name,
                )
 end
